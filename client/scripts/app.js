8a5a07678825ccb9df694d0852d0162192b9168b
chike17 <chike.onuorah@gmail.com>
2017-01-03 16:32:03 -0800

    cleanse messages and attempt new tab rooms with prev data

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -11,6 +11,8 @@ app.init = function () {
 
     $('.roomSelect').change(app.displayMessages);
 
+    $('#newTab').change(app.displayRoomNewTab);
+
     $('#main').on('click', app.handleUsernameClick);
 
     $('#send .submit').submit(app.handleSubmit);
@@ -33,6 +35,9 @@ app.init = function () {
     }); 
   });
   window.friends = [];
+  // if (window.childRoom) {
+  //   window.childRoom();
+  // }
   setInterval(app.fetch, 1000);
 };
 
@@ -66,6 +71,7 @@ app.fetch = function () {
       window.allMsgs = data.results;
       app.displayMessages();
       app.displayRooms();
+      app.findTrolls();
       $('.username').on('click', app.handleUsernameClick);
       window.friends.forEach(function(friend) {
         $('.' + friend).css('font-weight', 'bolder');
@@ -78,9 +84,29 @@ app.fetch = function () {
   });
 };
 
+app.cleanseMessage = function (message, objectId) {
+
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/messages/' + objectId,
+    type: 'PUT',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+      console.log(data);
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+  //  revise AJAX request to update post
+
+};
+
 app.server = 'https://api.parse.com/1/classes/messages'; // revisit
 
-app.clearMessages = function () {
+app.clearMessages = function (objectId) {
   $('#chats').empty();
 };
 
@@ -151,6 +177,33 @@ app.displayRooms = function() {
     $roomname.appendTo($('.roomSelect'));
   }
 };
+
+app.findTrolls = function() {
+  window.allMsgs.forEach(function(message) {
+    if (message.text !== undefined && message.text.indexOf('<') > -1 && message.text.indexOf('>') > -1) {
+      console.log(message.text);
+      var objectId = message.objectId;
+      var newMessage = {
+        username: message.username,
+        text: 'protected by SC Inc.',
+        roomname: message.roomname
+      };
+      app.cleanseMessage(newMessage, objectId);
+    }
+  });
+};
+
+// app.displayRoomNewTab = function() {
+//   var room = $('.roomSelect').val();
+//   var friends = window.friends;
+//   $('.roomSelect').change(function() {
+//     var newTab = window.open('index.html' + window.location.search, '_blank');
+//     newTab.friends = friends;
+//     newTab.childRoom = function() {
+//       $('.roomSelect').val(room); 
+//     };
+//   });
+// };
 
 app.init();
 
